abb1cbbe164d5dcc1053e4d3b35c040857564ae8
king <king@qq.com>
2021-03-25 18:42:05 +0800

    购物车

--- redisson-config/src/main/java/com/kq/redisson/util/Util.java
+++ redisson-config/src/main/java/com/kq/redisson/util/Util.java
@@ -2,6 +2,8 @@ package com.kq.redisson.util;
 
 public class Util {
 
-    public static final String REDIS_SERVER_URL = "192.168.99.100:6379";
+//    public static final String REDIS_SERVER_URL = "192.168.99.100:6379";
+    public static final String REDIS_SERVER_URL = "172.16.67.21:6379";
+//    public static final String REDIS_SERVER_URL = "172.16.5.12:6379";
 
 }
---
+++ shopping-cart/src/main/java/com/kq/shopping/cart/ShoppingCartApplication.java
@@ -0,0 +1,29 @@
+package com.kq.shopping.cart;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+    protected static Logger logger = LoggerFactory.getLogger(ShoppingCartApplication.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ShoppingCartApplication.class, args);
+
+        String[] beanNames = context.getBeanDefinitionNames();
+
+        for(String beanName : beanNames) {
+            logger.info("load beanName ={}",beanName);
+        }
+
+        logger.info("load beanNames size ={}",context.getBeanDefinitionCount());
+
+    }
+
+}
